f22e4bddb24e7360ee16e4631f0be63ffb425af3
Hoang Ngoc The <hoangthe8944@gmail.com>
2025-12-18 14:39:13 +0700

    first commit

--- beatboxweb/api/apiclient.ts
+++ beatboxweb/api/apiclient.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // --- CẤU HÌNH URL ---
 // Sử dụng process.env để linh hoạt hơn giữa môi trường dev và production
 const PUBLIC_URL = 'https://suzette-triform-angelina.ngrok-free.dev/api/public';
-const HiStory_URL = 'https://suzette-triform-angelina.ngrok-free.dev/api/songs';
+const History_URL = 'https://suzette-triform-angelina.ngrok-free.dev/api/songs';
 
 
 // ====================================================
@@ -75,7 +75,7 @@ export const getAllPublicSongs = () => {
 };
 export const getRecentlyPlayedSongs = () => {
     const token = localStorage.getItem("accessToken");
-    return axios.get<Song[]>(`${HiStory_URL}/history/songs`, { // Giả sử endpoint của bạn là thế này
+    return axios.get<Song[]>(`${History_URL}/history/recent`, { // Giả sử endpoint của bạn là thế này
          headers: {
             "ngrok-skip-browser-warning": "true",
             "Authorization": `Bearer ${token}` // Endpoint này yêu cầu xác thực
